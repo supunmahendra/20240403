7ae65961d2ea77df0109818af442816c63760b8d
Supun Mahendra <supunmahendra@gmail.com>
2024-04-03 13:57:39 +0200

    serch bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <h1>My FooDi</h1>
+        <div>
+            Find the <span>best</span> recipes
+        </div>
+    </nav>
+
+  )
+}
---
+++ src/components/Search.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export const Search = () => {
+  return (
+    <div>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+        <div className='searchContainer'>
+            <div>ssss</div>
+            <div className='search'>
+            <span class="searchIcon material-symbols-outlined">search</span>
+            <input type="text" id='searchInput' placeholder='Search' className='searchText'/>
+            </div>
+            <div>ssss</div>
+
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
