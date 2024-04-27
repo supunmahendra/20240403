2d07f4bed8e699943c6b173c46687ae673cb1e94
Supun Mahendra <supunmahendra@gmail.com>
2024-04-27 15:10:50 +0200

    final

--- src/components/WriteOneMenue.jsx
+++ src/components/WriteOneMenue.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
 export const WriteOneMenue = ({oneselectMeal, A, isToggled}) => {
+    let ingredients = [];
     
-    
+    for (let i = 1; i <= 2; i++) {
+        let ingredient = oneselectMeal["strMeal" + i];
+        console.log(ingredient);
+        if (ingredient) {
+            ingredients.push(ingredient);
+        }
+    }
+
+    console.log(ingredients);
+
   return (
-    <>
-    {
+    <> 
+    { 
+        
+
+
         oneselectMeal?(
             A === 2 && !isToggled ?(
                 oneselectMeal.map((item)=>(
@@ -17,10 +30,19 @@ export const WriteOneMenue = ({oneselectMeal, A, isToggled}) => {
                                     <div className='discription'>
                                         <h1 className='h1'>{item.strMeal}</h1>
                                         <p>From {item.strArea}</p>
+                                        <p>Ingridients:</p>
+                                        <div >
+                                            {[...Array(20).keys()].map((i) => (
+                                                item["strIngredient" + (i + 1)]&& (
+                                                <p className='i' key={i}>{i+1}.{ item["strIngredient" + (i + 1)]}</p>
+                                                )
+                                            ))}
+                                        </div>
+                                            
                                     </div>
                                 </div>
                                 <h1 className='h1'>preparation method</h1>
-                                <p>From {item.strInstructions}</p>
+                                <p> {item.strInstructions}</p>
                   </div>
                 ))):( isToggled && A === 1 &&
                     oneselectMeal.map((item)=>(
@@ -33,10 +55,17 @@ export const WriteOneMenue = ({oneselectMeal, A, isToggled}) => {
                                 <div className='discription'>
                                     <h1 className='h1'>{item.strDrink}</h1>
                                     <p> {item.strAlcoholic} <sapn className='h2'> {item.strCategory}</sapn></p>
+                                    <p>Ingridients:</p>
+                                            {[...Array(20).keys()].map((i) => (
+                                                item["strIngredient" + (i + 1)]&& (
+                                                    <p className='i' key={i}>{i+1}.{ item["strIngredient" + (i + 1)]}</p>
+                                                )
+                                            
+                                            ))}
                                 </div>
                             </div>
                             <h1 className='h1'>preparation method</h1>
-                            <p>From {item.strInstructions}</p>
+                            <p>{item.strInstructions}</p>
                     </div>
                 )))     
           ):(
